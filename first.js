5ba626f9dccc99464c14ea29297c474dbe5e014f
Chertovka <54009876+Chertovka@users.noreply.github.com>
2020-06-24 13:00:26 +0300

    Айтикод 2ая смена HTML, CSS, JS
    Айтикод 2ая смена HTML, CSS, JS

---
+++ first.js
@@ -0,0 +1,28 @@
+/*var a = 2;
+var b = 3;
+alert(a**b);
+
+var counter = 5;
+counter++;
+counter--;
+alert(counter);
+
+var a = confirm("Сегодня среда?");
+
+var a = 6;
+if(a==6)
+	alert("Переменная а = 6! ");
+else
+	alert("Переменная а не равна 6 и не равна 5!");*/
+
+/*var age = (19<18)?19:18;
+alert(age);*/
+
+var age = 16;
+if(age!!=15){
+	alert(age);
+}
+else
+	alert("Мне не 16!");
+
+
